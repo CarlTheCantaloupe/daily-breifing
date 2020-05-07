d217dc7361033b52216f689e71387f7b618c575d
Oscar Archibald <oscar_archibald@hotmail.com>
2020-05-07 18:01:43 +0100

    Added todo reminder

--- .dailyPicker.py
+++ .dailyPicker.py
@@ -1,71 +1,92 @@
-#This script finds a semi-random combination of GTK and Icon theme to be used every day.
+# This script finds a semi-random combination of GTK and Icon theme to be used every day.
 
-import random #Importing the random module so that we can make random numbers.
+# Importing the random module so that we can make random numbers.
+import random
 for x in range(1):
-  i = random.randint(0,13) #Finding a random number for the icons
+    i = random.randint(0, 13)  # Finding a random number for the icons
 
 for x in range(1):
-  n = random.randint(0,9) #Finding a random number for the themes
+    n = random.randint(0, 9)  # Finding a random number for the themes
 
 themes = [
-'Bazik',
-'Materia-Purple',
-'Nordic',
-'Pop',
-'Flat-Green',
-'Vimix',
-'Yaru',
-'Mojave',
-'Qogir',
-'Prof-GNOME'] #Listing all the themes we have to choose from
+    'Bazik',
+    'Materia-Purple',
+    'Nordic',
+    'Pop',
+    'Flat-Green',
+    'Vimix',
+    'Yaru',
+    'Mojave',
+    'Qogir',
+    'Prof-GNOME']  # Listing all the themes we have to choose from
 
 icons = [
-'Flatery',
-'Marwaita',
-'Numix',
-'Numix Circle',
-'Oranchelo',
-'Paper',
-'Papirus',
-'Tela',
-'Tela Circle',
-'Vimix',
-'Vitas',
-'Zafiro',
-'Candy',
-'Mojave'] #Listing all the icons we have to choose from
+    'Flatery',
+    'Marwaita',
+    'Numix',
+    'Numix Circle',
+    'Oranchelo',
+    'Paper',
+    'Papirus',
+    'Tela',
+    'Tela Circle',
+    'Vimix',
+    'Vitas',
+    'Zafiro',
+    'Candy',
+    'Mojave']  # Listing all the icons we have to choose from
 
-chosenIcon = icons[i] #This variable is the chosen icon theme
-chosenTheme = themes[n] #This variable is the chosen GTK theme
+chosenIcon = icons[i]  # This variable is the chosen icon theme
+chosenTheme = themes[n]  # This variable is the chosen GTK theme
 
 if chosenTheme == 'Nordic':
- chosenIcon = 'Zafiro' #The Nordic theme only goes with the Zafiro icon set, so we override the computer's choice of icons if the chosen theme is Nordic.
+    # The Nordic theme only goes with the Zafiro icon set, so we override the computer's choice of icons if the chosen theme is Nordic.
+    chosenIcon = 'Zafiro'
 
-if chosenTheme ==  'Qogir':
-  chosenIcon = 'Qogir' #The Qogir icon theme only goes with the Qogir GTK theme, so if the Qogir GTK theme gets selected, the Qogir icons will accompany.
+if chosenTheme == 'Qogir':
+    # The Qogir icon theme only goes with the Qogir GTK theme, so if the Qogir GTK theme gets selected, the Qogir icons will accompany.
+    chosenIcon = 'Qogir'
 
 while chosenTheme == 'Pop' and chosenIcon == 'Zafiro':
-  for x in range(1):
-    i = random.randint(0,14)
+    for x in range(1):
+        i = random.randint(0, 14)
 
-  for x in range(1):
-    n = random.randint(0,8)
+    for x in range(1):
+        n = random.randint(0, 8)
 
-  chosenIcon = icons[i]
-  chosenTheme = themes[n] #Pop and Zafiro don't go well together, so we redo the random selection process as long as they are the chosen pair. This can be expanded in the future.
+    chosenIcon = icons[i]
+    # Pop and Zafiro don't go well together, so we redo the random selection process as long as they are the chosen pair. This can be expanded in the future.
+    chosenTheme = themes[n]
 
 while chosenTheme == 'Yaru' and chosenIcon == 'Zafiro':
-  for x in range(1):
-    i = random.randint(0,14)
+    for x in range(1):
+        i = random.randint(0, 14)
 
-  for x in range(1):
-    n = random.randint(0,8)
+    for x in range(1):
+        n = random.randint(0, 8)
 
-  chosenIcon = icons[i]
-  chosenTheme = themes[n] #Yaru and Zafiro don't go well together, so we redo the random selection process as long as they are the chosen pair. This can be expanded in the future.
+    chosenIcon = icons[i]
+    # Yaru and Zafiro don't go well together, so we redo the random selection process as long as they are the chosen pair. This can be expanded in the future.
+    chosenTheme = themes[n]
 
-outputThemes = "Today's GTK and Shell Theme: " + chosenTheme #Creating a nice human-readable string.
-outputIcons = "Today's Icon Theme: " + chosenIcon #Creating a nice human-readable string
+# Creating a nice human-readable string.
+outputThemes = "Today's GTK and Shell Theme: " + chosenTheme
+# Creating a nice human-readable string
+outputIcons = "Today's Icon Theme: " + chosenIcon
+
+toDo = ''' 
+Chemistry Worksheet on salt preparation
+French Finishing Off - check on firefly
+Geography Topic 4 - in advance
+Maths Prep - 😭
+Art - Dazzle designs without colour
+English - today's work 😭
+'''
+# Insert the list of things to do here
+
+toDoFinal = "Things to do soon: " + toDo  # Create the todo string to be output
 
 print(outputThemes)
-print(outputIcons) #Printing the results
+print(outputIcons)  # Printing the results
+print()  # Empty line
+print(toDoFinal)  # Print the things I have to do
